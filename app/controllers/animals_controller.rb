9dc500c44a888a3e84061f611e3217b3306754b2
VadymVykhodtsev <vadymvykhodtsev@gmail.com>
2026-03-17 13:36:36 +0200

    added controller $ view

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,70 +1,16 @@
 class AnimalsController < ApplicationController
-  before_action :set_animal, only: %i[ show edit update destroy ]
-
-  # GET /animals or /animals.json
   def index
+    filters = filter_params
+
     @animals = Animal.all
-  end
-
-  # GET /animals/1 or /animals/1.json
-  def show
-  end
-
-  # GET /animals/new
-  def new
-    @animal = Animal.new
-  end
-
-  # GET /animals/1/edit
-  def edit
-  end
-
-  # POST /animals or /animals.json
-  def create
-    @animal = Animal.new(animal_params)
-
-    respond_to do |format|
-      if @animal.save
-        format.html { redirect_to @animal, notice: "Animal was successfully created." }
-        format.json { render :show, status: :created, location: @animal }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @animal.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /animals/1 or /animals/1.json
-  def update
-    respond_to do |format|
-      if @animal.update(animal_params)
-        format.html { redirect_to @animal, notice: "Animal was successfully updated.", status: :see_other }
-        format.json { render :show, status: :ok, location: @animal }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @animal.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /animals/1 or /animals/1.json
-  def destroy
-    @animal.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to animals_path, notice: "Animal was successfully destroyed.", status: :see_other }
-      format.json { head :no_content }
-    end
+    @animals = @animals.by_species(filters[:species]) if filters[:species].present?
+    @animals = @animals.age_less_than(filters[:age_limit]) if filters[:age_limit].present?
+    @animals = @animals.by_sex(filters[:sex]) if filters[:sex].present?
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_animal
-      @animal = Animal.find(params.expect(:id))
-    end
 
-    # Only allow a list of trusted parameters through.
-    def animal_params
-      params.expect(animal: [ :species, :age, :sex ])
-    end
+  def filter_params
+    params.permit(:species, :age_limit, :sex)
+  end
 end
