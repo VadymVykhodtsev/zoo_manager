4b0db034cbeded9c3c29f4aaf0d573be1aaff5fe
VadymVykhodtsev <vadymvykhodtsev@gmail.com>
2026-03-17 13:20:36 +0200

    added seeds for animals

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,12 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Animal.destroy_all
+
+animals = [
+  { species: 'monkey', age: 3, sex: 'male' },
+  { species: 'squirrel', age: 5, sex: 'female' },
+  { species: 'monkey', age: 1, sex: 'male' },
+  { species: 'monkey', age: 11, sex: 'male' },
+  { species: 'lion', age: 7, sex: 'male' }
+]
+
+Animal.create!(animals)
+puts "Created #{Animal.count} animals"
