a41e457c027cdfce395b007c77db016c738f0f47
VadymVykhodtsev <vadymvykhodtsev@gmail.com>
2026-03-17 13:21:49 +0200

    model for animals

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,11 @@
 class Animal < ApplicationRecord
+  scope :by_species, ->(kind) { where(species: kind) if kind.present? }
+  scope :age_less_than, ->(years) { where('age < ?', years) if years.present? }
+  scope :by_sex, ->(gender) { where(sex: gender) if gender.present? }
+
+  scope :monkeys, -> { by_species('monkey') }
+  scope :males,   -> { by_sex('male') }
+  scope :females, -> { by_sex('female') }
+
+  validates :species, :age, :sex, presence: true
 end
